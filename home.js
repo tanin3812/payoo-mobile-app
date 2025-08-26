4153a8859c192286638370fe58c6dd445c2c6d36
tanin3812 <tanin15-3812@diu.edu.bd>
2025-08-27 04:14:59 +0600

    implement pin and account number

--- home.js
+++ home.js
@@ -1,14 +1,25 @@
+const validPin = 1234
+
 document.getElementById('addMoney').addEventListener('click', function(e){
     e.preventDefault()
-    console.log('btn clicked')
 
     const bank = document.getElementById('bank').value
     const accountNum = document.getElementById('accountNum').value
     const amount = parseInt(document.getElementById('addAmount').value)
-    const pin = document.getElementById('pinNumber').value
+    const pin = parseInt(document.getElementById('pinNumber').value)
 
     const availableBalance = parseInt(document.getElementById('availableBalance').innerText)
     console.log(availableBalance)
+
+    if(accountNum.length < 11){
+        alert('Please provide valid account number')
+        return;
+    }
+
+    if(pin !== validPin){
+        alert('Please provide correct pin number')
+        return;
+    }
 
     const totalNewBalance = amount+availableBalance
     document.getElementById('availableBalance').innerText = totalNewBalance
