1235213666e17b1c946b6bbebdec55d881027427
tanin3812 <tanin15-3812@diu.edu.bd>
2025-08-27 05:07:13 +0600

    implemented cashout feature and toggle

--- home.js
+++ home.js
@@ -1,5 +1,6 @@
 const validPin = 1234
 
+// add money feature
 document.getElementById('addMoney').addEventListener('click', function(e){
     e.preventDefault()
 
@@ -23,4 +24,25 @@ document.getElementById('addMoney').addEventListener('click', function(e){
 
     const totalNewBalance = amount+availableBalance
     document.getElementById('availableBalance').innerText = totalNewBalance
+})
+
+// cashout feature
+document.getElementById('withdraw-btn').addEventListener('click', function(e){
+    e.preventDefault()
+    const amount = parseInt(document.getElementById('withdrawAmount').value)
+
+    const availableBalance = parseInt(document.getElementById('availableBalance').innerText)
+
+    const totalNewBalance = availableBalance - amount
+    document.getElementById('availableBalance').innerText = totalNewBalance
+})
+
+// toggling featurs
+document.getElementById('add-button').addEventListener('click', function(){
+    document.getElementById('cash-out-parent').style.display = 'none'
+    document.getElementById('add-money-parent').style.display = 'block'
+})
+document.getElementById('cashout-button').addEventListener('click', function(){
+    document.getElementById('add-money-parent').style.display = 'none'
+    document.getElementById('cash-out-parent').style.display = 'block'
 })
